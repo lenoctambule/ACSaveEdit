9415ad188c5f0fe384a14a95fb3bf6666d066c44
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-05-31 16:50:22 +0200

    Fix bad rotation offset

--- acsaveedit.py
+++ acsaveedit.py
@@ -10,6 +10,7 @@ def gen_key(id):
 
 def rrotate(key, n):
 	rotated = b""
+	print(f"Rotation\t: {n}")
 	for i in range(len(key)):
 		off = (i + n) % len(key)
 		rotated += key[off:off+1]
@@ -23,11 +24,12 @@ if __name__ == "__main__" :
 		print(f"ID used\t: {sys.argv[2]}")
 		data	= f.read()
 		ct_data = data[0x228:]
-		key		= rrotate(gen_key(sys.argv[2]), len(ct_data) % 16)
+		key		= rrotate(gen_key(sys.argv[2]), len(ct_data) % 16 - 1)
 		print(f"Rotated key\t: {key.hex()}")
 		print(f"Data length\t: {len(ct_data)}")
 		with open(sys.argv[1] + ".hkd", "wb") as out :
 			i = 0
+			out.write(data[:0x228])
 			for b in ct_data :
 				out.write(bytes([b ^ key[i % 16]]))
 				i += 1
